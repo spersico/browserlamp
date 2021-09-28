5c85d3dcff5114b96ada49b51ec8dbb7461c9a1f
Federico Polesel <pole@MacBook-Pro.home>
2021-09-28 17:08:19 +0200

    First Workaround

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,52 @@
-import { createSignal } from "solid-js"
-import { render } from "solid-js/web"
+import { createSignal } from 'solid-js';
+import { render } from 'solid-js/web';
 
-import "./index.css"
-import { colors, coloredLog } from "./colors"
-import MainSwitch from "./components/MainSwitch"
-import ColorChanger from "./components/ColorChanger"
-import Footer from "./components/Footer"
+import './index.css';
+import { colors, coloredLog } from './colors';
+import MainSwitch from './components/MainSwitch';
+import ColorChanger from './components/ColorChanger';
+import Footer from './components/Footer';
 
 function Home() {
-  const [globalColor, setGlobalColor] = createSignal(colors[0].value)
-  const [lastColor, setLastColor] = createSignal(colors[1].value)
-  const [lampIsOn, setLampSwitch] = createSignal(false)
+  const [globalColor, setGlobalColor] = createSignal(colors[0].value);
+  const [lastColor, setLastColor] = createSignal(colors[1].value);
+  const [lampIsOn, setLampSwitch] = createSignal(false);
 
   const onChangeGlobalColor = (newColor) => {
-    setLampSwitch(newColor !== colors[0].value)
+    setLampSwitch(newColor !== colors[0].value);
     if (globalColor() !== colors[0].value && lastColor() !== globalColor()) {
-      setLastColor(globalColor())
+      setLastColor(globalColor());
     }
-    setGlobalColor(newColor)
-    coloredLog("last", lastColor())
-    coloredLog("global", globalColor())
-  }
+    setGlobalColor(newColor);
+    coloredLog('last', lastColor());
+    coloredLog('global', globalColor());
+  };
+
+  const pickerHexToRgb = (hex) => {
+    hex = hex.replace(/#/g, '');
+    if (hex.length === 3) {
+      hex = hex
+        .split('')
+        .map(function (hex) {
+          return hex + hex;
+        })
+        .join('');
+    }
+    // validate hex format
+    let result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(
+      hex
+    );
+    if (result) {
+      let red = parseInt(result[1], 16);
+      let green = parseInt(result[2], 16);
+      let blue = parseInt(result[3], 16);
+
+      const hexColor = `${red},${green},${blue}`;
+      onChangeGlobalColor(hexColor);
+    } else {
+      return null;
+    }
+  };
 
   return (
     <main style={{ background: `rgb(${globalColor()})` }}>
@@ -33,9 +59,16 @@ function Home() {
         globalColor={globalColor}
         onChangeColor={onChangeGlobalColor}
       />
+      <input
+        type='color'
+        value={globalColor}
+        onInput={(e) => {
+          pickerHexToRgb(e.target.value);
+        }}
+      />
       <Footer />
     </main>
-  )
+  );
 }
 
-render(Home, document.getElementById("root"))
+render(Home, document.getElementById('root'));
